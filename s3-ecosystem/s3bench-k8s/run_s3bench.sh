bbecb73de4004e9d58b80733d1cfb15ef76404d1
Shon Paz <shonpaz123@gmail.com>
2020-02-11 20:49:52 +0200

    added k8s demo

---
+++ s3-ecosystem/s3bench-k8s/run_s3bench.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+AccessKey=`kubectl get secret rook-ceph-object-user-my-store-shon -n rook-ceph -o 'jsonpath={.data.AccessKey}' | base64 --decode`
+SecretKey=`kubectl get secret rook-ceph-object-user-my-store-shon -n rook-ceph -o 'jsonpath={.data.SecretKey}' | base64 --decode`
+sed -i "s/ACCESS_KEY/${AccessKey}/g" s3bench.yaml
+sed -i "s/SECRET_KEY/${SecretKey}/g" s3bench.yaml
+
+kubectl create -f s3bench.yaml
+kubectl wait --for=condition=complete --timeout=600s job/s3bench
+
+echo "Done!"
